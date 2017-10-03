f660f15b0206079665a9e47885b10435fd78b91b
Hanna Nguyen <ha.nguyenthidan@gmail.com>
2017-10-03 17:42:11 -0500

    Completed Object Challenge

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+var Region = function (state, revenue, locationCount, averageRevenue) {
+    this.state = state;
+    this.revenue = revenue;
+    this.locationCount = locationCount;
+    this.averageRevenue = function () {
+        var avg = this.revenue/this.locationCount;
+        return avg;
+
+    };
+
+};
+
 
 // object instances
+var Illinois = new Region ('IL', 5000, 12);
+var Minnesota = new Region ('MN', 300, 3);
+var Nevada = new Region ('NV', 25000, 1);
 
 // push object instances to locations array
+locations.push(Illinois, Minnesota, Nevada);
+
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
